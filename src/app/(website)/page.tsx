e1a28ff4c7f123fa071a1ec7ab4a2b120ab40f94
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-02 11:48:35 +0600

    brand and trade section done

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/Home/Hero";
+import { TradeAdvantageSection } from "@/components/Home/TradeAdvantageSection";
+import TrustedBrands from "@/components/Home/TrustedBrands";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <TrustedBrands />
+      <TradeAdvantageSection />
     </div>
   );
 };
---
+++ src/components/Home/TrustedBrands.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+
+export default function TrustedBrands() {
+  const brands = [
+    { name: "TheTradeDesk", logo: "/brand/brand1.png" },
+    { name: "Mercado Libre", logo: "/brand/brand2.png" },
+    { name: "Mops", logo: "/brand/brand3.png" },
+    { name: "DoorDash", logo: "/brand/brand4.png" },
+    { name: "Criteo", logo: "/brand/brand5.png" },
+    { name: "TikTok Shop", logo: "/brand/brand6.png" },
+    { name: "TheTradeDesk", logo: "/brand/brand7.png" },
+    { name: "Mercado Libre", logo: "/brand/brand8.png" },
+    { name: "Mops", logo: "/brand/brand9.png" },
+    { name: "DoorDash", logo: "/brand/brand10.png" },
+    { name: "TheTradeDesk", logo: "/brand/brand1.png" },
+    { name: "Mercado Libre", logo: "/brand/brand2.png" },
+    { name: "Mops", logo: "/brand/brand3.png" },
+    { name: "DoorDash", logo: "/brand/brand4.png" },
+  ];
+
+  return (
+    <section className="text-center bg-[#1c2228] py-14">
+
+      <h2 className="text-3xl font-semibold text-white mb-10">
+        &quot;Trusted by Industry Leaders&quot;
+      </h2>
+
+      <Marquee speed={50} gradient={false} pauseOnHover>
+        {brands.map((brand, index) => (
+          <Image
+            key={index}
+            src={brand.logo}
+            alt={brand.name}
+            width={1000}
+            height={1000}
+            className="mx-6 h-20 w-auto object-contain"
+          />
+        ))}
+      </Marquee>
+    </section>
+  );
+}
